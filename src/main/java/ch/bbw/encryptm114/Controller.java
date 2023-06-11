e55673db5ebe2c0dbd3bd4c0acea71b43c4796e6
Lorenz Hohermuth <lorenz.hohermuth@bsi-software.com>
2023-06-11 14:54:29 +0200

    Setup Project

---
+++ src/main/java/ch/bbw/encryptm114/Controller.java
@@ -0,0 +1,23 @@
+package ch.bbw.encryptm114;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    public TextField input;
+    @FXML
+    public CheckBox salt;
+    @FXML
+    public CheckBox pepper;
+    @FXML
+    private Label outText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        outText.setText(String.valueOf(salt.isSelected()));
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ch.bbw.encryptm114 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ch.bbw.encryptm114 to javafx.fxml;
+    exports ch.bbw.encryptm114;
+}
