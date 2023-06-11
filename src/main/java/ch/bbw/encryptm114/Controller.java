8af1b35638040d6e2457d9d253874a4dacb40c50
Lorenz Hohermuth <lorenz.hohermuth@bsi-software.com>
2023-06-11 19:51:39 +0200

    1.3.0 XOR encryption + salt and pepper

--- src/main/java/ch/bbw/encryptm114/Controller.java
+++ src/main/java/ch/bbw/encryptm114/Controller.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import java.util.Random;
 
 public class Controller {
 
@@ -22,13 +23,13 @@ public class Controller {
             return;
         }
         if (xor.isSelected()) {
-            outText.setText("xor");
+            message = XOREncrypt(message);
         }
         if (salt.isSelected()) {
-            outText.setText("salt");
+            message = getSalt() + message;
         }
         if (pepper.isSelected()) {
-            outText.setText("pepper");
+            message = message + getSalt(); //salt identifies as Pepper
         }
         if (hash.isSelected()) {
             message = hashMessage(message);
@@ -36,11 +37,25 @@ public class Controller {
         outText.setText(message);
     }
 
+    String XOREncrypt(String text) {
+        String binText = toBinaryString(text);
+        String binKey = generateKey(binText.length());
+        String encryptedText = XORString(binText, binKey, binText.length());
+        String out = binaryToText(encryptedText);
+        return out;
+    }
+
     String hashMessage(String text) {
         String binText1 = toBinaryString(text);
         String binText2 = flipString(binText1);
         String XORBinText = XORString(binText1, binText2, binText2.length());
         String out = binaryToText(XORBinText);
+        return out;
+    }
+
+    String getSalt() {
+        String binSalt = generateRandomBin(3*8);
+        String out = binaryToText(binSalt);
         return out;
     }
 
@@ -93,5 +108,19 @@ public class Controller {
     String[] splitInPairsOf(String text, int n) {
         String[] results = text.split("(?<=\\G.{" + n + "})");
         return results;
+    }
+
+    String generateKey(int length) {
+        return generateRandomBin(length);
+    }
+
+    String generateRandomBin(int length) {
+        String out = "";
+        Random rand = new Random();
+        for (int i = 0; i < length; i++) {
+            int randInt = rand.nextInt(2);
+            out += randInt;
+        }
+        return out;
     }
 }
