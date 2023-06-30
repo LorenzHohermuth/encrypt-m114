00c111bda1128f7d6bee2483519b87e3be94f570
Lorenz Hohermuth <lorenz.hohermuth@bsi-software.com>
2023-06-30 14:36:31 +0200

    1.9.1 Added Icon

--- src/main/java/ch/bbw/encryptm114/App.java
+++ src/main/java/ch/bbw/encryptm114/App.java
@@ -3,9 +3,12 @@ package ch.bbw.encryptm114;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 
 public class App extends Application {
     @Override
@@ -14,6 +17,9 @@ public class App extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 400, 800);
         stage.setTitle("Encrypt your message");
         stage.setScene(scene);
+        URL imagesUrl = getClass().getResource("images/icon.png");
+        Image icon = new Image(imagesUrl.toString());
+        stage.getIcons().add(icon);
         stage.show();
     }
 
